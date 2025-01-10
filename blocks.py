b7146613973fed7496c9d1082404e28754889007
Seth Landry <waymakerijn@outlook.com>
2025-01-09 18:52:02 -0600

    Add a method to merge the specified dependabot branch
    * **New method**: `merge_dependabot_branch`
      - Import `os` module
      - Checkout the specified dependabot branch
      - Merge the main branch into the dependabot branch
      - Push the changes to the dependabot branch

--- blocks.py
+++ blocks.py
@@ -10,3 +10,9 @@ class Block:
     def handle_paytr_wallet_issue(self):
         # Handle the paytr-wallet issue
         pass
+
+    def merge_dependabot_branch(self):
+        import os
+        os.system("git checkout dependabot/npm_and_yarn/server/npm_and_yarn-6b7e5c81f3")
+        os.system("git merge main")
+        os.system("git push -u origin dependabot/npm_and_yarn/server/npm_and_yarn-6b7e5c81f3")
